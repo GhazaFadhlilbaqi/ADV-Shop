cdb55d5e2f11b7f198a65a6e984dc2eb56ea9028
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-02-09 16:26:06 +0700

    feat: Added delete product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.model.Product;
-import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
 
 @Repository
 public class ProductRepository {
@@ -36,5 +37,9 @@ public class ProductRepository {
             }
         }
         return null;
+    }
+
+    public boolean delete(String id) {
+        return productData.removeIf(product -> product.getProductId().equals(id));
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -1,13 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
-import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -36,5 +37,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Product findById(String id) {
         return productRepository.findById(id);
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return productRepository.delete(id);
     }
 }
