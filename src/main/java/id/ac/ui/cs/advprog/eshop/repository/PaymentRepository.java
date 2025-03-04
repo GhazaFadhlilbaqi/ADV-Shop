5670d76c9d77d6480c879b58c99af68a8ad6bda4
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-03-04 20:08:04 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,19 +1,37 @@
 package id.ac.ui.cs.advprog.eshop.repository;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-
 @Repository
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
-    public Payment save(Payment payment) {return null;}
+    public Payment save(Payment payment) {
+        for (int i=0; i<paymentData.size(); i++) {
+            Payment p = paymentData.get(i);
+            if (p.getId().equals(payment.getId())){
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+        }
+        paymentData.add(payment);
+        return payment;
+    }
 
-    public Payment findById(String id) {return null;}
+    public Payment findById(String id) {
+        for (Payment p : paymentData) {
+            if (p.getId().equals(id)) {
+                return p;
+            }
+        }
+        return null;
+    }
 
-    public List<Payment> findAll() {return null;}
+    public List<Payment> findAll() {
+        return paymentData;
+    }
 }
