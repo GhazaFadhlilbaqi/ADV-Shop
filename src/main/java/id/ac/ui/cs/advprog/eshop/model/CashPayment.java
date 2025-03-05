48fae71ec5bbc72e67517377b790289dadccac96
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-03-05 15:04:03 +0700

    [GREEN] Implemented COD Payment class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/CashPayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/CashPayment.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 
 public class CashPayment extends Payment {
+    private static final String ADDRESS_KEY = "address";
+    private static final String DELIVERY_FEE_KEY = "deliveryFee";
     
     public CashPayment(String id, String method, String status, Map<String, String> paymentData) {
         super(id, method, status, paymentData);
@@ -15,6 +17,25 @@ public class CashPayment extends Payment {
     }
 
     public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        } else {
+            String address = paymentData.get(ADDRESS_KEY);
+            String deliveryFee = paymentData.get(DELIVERY_FEE_KEY);
+            
+            if (isValidPaymentData(address, deliveryFee)) {
+                setStatus(PaymentStatus.SUCCESS.getDisplayName());
+            } else {
+                setStatus(PaymentStatus.REJECTED.getDisplayName());
+            }
+        }
     }
 
+    private boolean isValidPaymentData(String address, String deliveryFee) {
+        return isNotNullOrEmpty(address) && isNotNullOrEmpty(deliveryFee);
+    }
+
+    private boolean isNotNullOrEmpty(String value) {
+        return value != null && !value.isEmpty();
+    }
 }
